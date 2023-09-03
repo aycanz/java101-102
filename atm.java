f9b53af312e66978f737669f7d8616a3284f4988
aycan acar <108690004+aycanz@users.noreply.github.com>
2023-09-03 14:37:45 +0300

    Add files via upload

---
+++ atm.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class atm{
+    public static void main(String[] args) {
+        String userName, password;
+        Scanner input = new Scanner(System.in);
+        int right = 3;
+        double balance = 1500.0;
+        int select;
+
+        while (right > 0) {
+            System.out.print("Kullanıcı Adınız: ");
+            userName = input.nextLine();
+            System.out.print("Parolanız: ");
+            password = input.nextLine();
+
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+                do {
+                    System.out.println("1-Para Yatırma\n" +
+                            "2-Para Çekme\n" +
+                            "3-Bakiye Sorgula\n" +
+                            "4-Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz: ");
+                    select = input.nextInt();
+                    switch (select) {
+                        case 1:
+                            System.out.print("Para miktarı: ");
+                            double price = input.nextDouble();
+                            balance += price;
+                            break;
+                        case 2:
+                            System.out.print("Para miktarı: ");
+                            double pricen = input.nextDouble();
+                            if (pricen > balance) {
+                                System.out.println("Bakiye yetersiz.");
+                            } else {
+                                balance -= pricen;
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Bakiyeniz: " + balance);
+                            break;
+                        case 4:
+                            System.out.println("Tekrar görüşmek üzere.");
+                            break;
+                        default:
+                            System.out.println("Geçersiz bir seçenek girdiniz.");
+                            break;
+                    }
+                } while (select != 4);
+                break;
+            } else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabınız bloke olmuştur. Lütfen banka ile iletişime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkınız: " + right);
+                }
+            }
+        }
+    }
+}
